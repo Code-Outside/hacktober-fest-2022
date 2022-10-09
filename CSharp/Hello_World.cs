ee729ede4784f3fc4ccb79189912495414286e38
SASWATA PANDIT <saspandit@microsoft.com>
2022-10-09 15:13:04 +0530

    Add CSharp Folder and Hello World

---
+++ CSharp/Hello_World.cs
@@ -0,0 +1,17 @@
+// C# program to print Hello World!
+using System;
+
+namespace HelloWorldApp {
+	
+	class HelloWorld {
+		
+		// Main Method
+		static void Main(string[] args) {
+			
+			// printing Hello World!
+			Console.WriteLine("Hello World!");
+			
+			Console.ReadKey();
+		}
+	}
+}
