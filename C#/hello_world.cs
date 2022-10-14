4f3e0cbcbe81b127807baba8c82bcdd76eccd749
ragu-1997 <81232337+ragu-1997@users.noreply.github.com>
2022-10-14 13:07:38 +0530

    helloworld for c#

---
+++ C#/hello_world.cs
@@ -0,0 +1,15 @@
+// C# program to print Hello World!
+using System;
+namespace HelloWorldApp {
+	class Geeks {
+		
+		static void Main(string[] args) {
+			
+			
+			// printing Hello World!
+			Console.WriteLine("Hello World!");
+			
+			Console.ReadKey();
+		}
+	}
+}
